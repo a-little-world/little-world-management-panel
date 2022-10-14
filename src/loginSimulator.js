6d62ed0cbcc3f5223147ba379de778f36b54aa28
Tim Schupp <tbscode@users.noreply.github.com>
2022-10-14 18:25:17 +0200

    added capability to supply test filters to the 'login_hack' api (#8)

--- src/loginSimulator.js
+++ src/loginSimulator.js
@@ -9,6 +9,13 @@ const data = {
   baseUrl: "http://localhost:3333"
 }
 
+const exampleFilters = {
+  allUsersSearching: "state.matching_state:is:1",
+  allUsersSearchingThatAreLearners: ["state.matching_state:is:1", "profile.learner:is:1"], // This is no-one user currently
+  allLearners : "profile.learner:is:1",
+  allVolunteers : "profile.learner:is:0",
+}
+
 export function simulatedAutoLogin(username=data.username, password=data.passoword) {
   return fetch(`${data.baseUrl}/api2/login_hack/`, {
     method: 'POST',
@@ -16,7 +23,12 @@ export function simulatedAutoLogin(username=data.username, password=data.passowo
     body: utils.objectToFormData({
       username,
       password,
-      "dev_dataset" : "admin_area"
+      "dev_dataset" : "admin_area",
+      /* 
+      Filters are handled via get-params in production, this is just to simulate the production behavior,
+      TODO: there is still an issue with lists not being delivered as real list using the `utils.objectToFormData`
+      */
+      "filter" : exampleFilters.allVolunteers
     })
   })
 }
