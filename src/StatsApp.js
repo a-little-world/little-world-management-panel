5995ca1f92f571d6b0e5c99326eba5e3e6598027
tim <tim@timschupp.de>
2023-01-17 17:22:36 +0100

    implemented siple graphs for the stats panel

--- src/StatsApp.js
+++ src/StatsApp.js
@@ -3,17 +3,25 @@ import { ThemeProvider } from 'styled-components';
 import { StatsDashboard } from './StatsDashboard';
 import theme from './theme';
 
-function StatsApp({ inputStats }) {
-  const [stats, setStats] = useState({});
+function StatsApp({ inputSeries, inputStaticStats }) {
+  const [series, setSeries] = useState({});
+  const [staticStats, setStaticStats] = useState({});
 
   useEffect(() => {
-    const newStats = inputStats || {};
-    setStats(newStats);
-  }, [inputStats]);
+    console.log('IN', inputSeries);
+    const newStats = inputSeries || {};
+    setSeries(newStats);
+  }, [inputSeries]);
+
+  useEffect(() => {
+    console.log('IN Static', inputStaticStats);
+    const newStats = inputStaticStats || {};
+    setStaticStats(newStats);
+  }, [inputStaticStats]);
 
   return (
     <ThemeProvider theme={theme}>
-      <StatsDashboard stats={stats} />
+      <StatsDashboard series={series} staticStats={staticStats} />
     </ThemeProvider>
   );
 }
--- src/StatsDashboard.jsx
+++ src/StatsDashboard.jsx
@@ -1,8 +1,41 @@
+import Plot from 'react-plotly.js';
+import { Table } from "./views/styles"
 
 export const StatsDashboard = ({
-    stats
+    series,
+    staticStats
 }) => {
     return (
-        <div>Hello stats</div>
+        <><h1> Little World Stats (Beta)</h1>
+        <div>(updated hourly)</div>
+        <Table>
+            <thead>
+                    <tr>
+                        <th>Absolute Registered Users</th>
+                        <th>Absolute matches made</th>
+                        <th>Count users currently online</th>
+                    </tr>
+            </thead>
+        <tbody>
+            <tr>
+                <td>{staticStats?.total_amoount_of_users}</td>
+                <td>{staticStats?.total_matches}</td>
+                <td>TODO</td>
+            </tr>
+        </tbody>
+        </Table>
+        {Object.keys(series).map(stat => {
+            return <>
+                <Plot
+                data={[
+                {type: 'bar', 
+                x: series[stat].map((elem) => elem.x), 
+                y: series[stat].map((elem) => elem.y)},
+                ]}
+                layout={ {width: 600, height: 400, title: stat} }
+            />
+            </>
+        })}
+        </>
     )
 }
