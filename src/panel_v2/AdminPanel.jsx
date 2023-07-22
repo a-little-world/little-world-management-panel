72c421be9250904a2096225da38076cdff5586a2
tim <tim@timschupp.de>
2023-07-22 15:03:52 +0200

    field names and images can now be rendered correctly!

--- src/panel_v2/AdminPanel.jsx
+++ src/panel_v2/AdminPanel.jsx
@@ -1,5 +1,6 @@
 import { Children, useState } from 'react'
 import '../withTailwind.css';
+import UserImage from '../atoms/userImage.jsx'
 
 
 const NavBar = () => {
@@ -17,6 +18,7 @@ const NavBar = () => {
 const baseTableStyles = "p-0 h-2"
 
 const FIELDS = {
+    //""
     "profile.user_type": (field) => (<td className={`${baseTableStyles}`}>{field}</td>),
     "profile.first_name": (field) => (<td className={`${baseTableStyles}`}>{field}</td>),
     "profile.second_name": (field) => (<td className={`${baseTableStyles}`}>{field}</td>),
@@ -39,21 +41,28 @@ function fetchFromObject(obj, prop){
     return obj[prop];
 }
 
-const Table = ({users, fields}) => {
+const Table = ({users, fields, selectedUsers, setSelectedUsers}) => {
+    console.log("SELECTED", selectedUsers)
       return <table className="table table-zebra leading-3 w-full z-50 bg-base-100">
         <thead className='bg-base-300'>
           <tr>
             <th></th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Favorite Color</th>
+            {fields.map((field, i) => {
+                return <th key={i}>{field}</th> 
+            })}
           </tr>
         </thead>
         <tbody>
-        {users.map((user,i) => {
-            return <tr key={i} className='p-0 hover:bg-accent hover:text-base-200'>
+        {users.results.map((user,i) => {
+            return <tr key={i} className='p-0 hover:bg-100 hover:text-success hover:border hover:border-accent'>
                 <th className='w-20'>
-                    <input type="checkbox" checked="checked" className="checkbox ml-2" />
+                    <input type="checkbox" checked={selectedUsers.indexOf(user.hash) != -1} className="checkbox ml-2" onChange={() => {
+                        if(selectedUsers.indexOf(user.hash) != -1) {
+                            setSelectedUsers(selectedUsers.filter((hash) => hash !== user.hash))
+                        } else {
+                            setSelectedUsers([...selectedUsers, user.hash])
+                        }
+                    }}/>
                 </th>
                 {fields.map((field, j) => {
                     return getTableComponentUser(user, field)
@@ -69,23 +78,36 @@ const getTableComponentUser = (user, field) => {
     return FIELDS[field](value)
 }
 
-const UserSelectionDrawer = ({selectedUsers, fields, children}) => {
-    return <div className='w-full h-full bg-error min-h-full max-h-full flex flex-row'>
-        <div className='w-3/4 h-full bg-info min-h-full max-h-full'>
-            {children}
+const UserDetailsCard = ({user}) => {
+    return <div className='w-full flex flex-col bg-error h-72'>
+        <div className='w-full h-32 bg-error'>
+           <UserImage user={user.profile} />
         </div>
-        <div className='w-1/4 h-full bg-accent min-h-full max-h-full'>
-            SIDE
+        <div className='w-full h-10 bg-info'>
+        Ho
         </div>
     </div>
 }
 
-const DynamicDisplay = ({querySets,selectedList, children}) => {
+const UserSelectionDrawer = ({selectedUsersHashes, selectedUsers, fields, children}) => {
+    return <div className='w-full h-full min-h-full max-h-full flex flex-row relative'>
+        <div className='w-3/4 h-screen min-h-full max-h-full overflow-y-scroll'>
+            {children}
+        </div>
+        <div className='w-1/4 h-full bg-accent min-h-full max-h-full'>
+            {selectedUsers.map((user, i) => {
+                return <UserDetailsCard user={user} />
+            })}
+        </div>
+    </div>
+}
+
+const DynamicDisplay = ({querySets, selectedList, children, selectedUsersHashes, selectedUsers}) => {
 
     return <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content">
-        <UserSelectionDrawer>
+        <UserSelectionDrawer selectedUsers={selectedUsers} selectedUsersHashes={selectedUsersHashes}>
             {children}
         </UserSelectionDrawer>
         <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
@@ -116,12 +138,30 @@ export const AdminPanel = ({
     _userLists
 }) => {
     
+    // A dict {list_name: <paginated-user-listing>}.results = [] ...
     const [userLists, setUserLists] = useState(_userLists)
+    // Just a string reference to the current list
     const [list, setList] = useState("all")
+    // Contains a list of selected users hashes from the current list
+    const [usersListsSelections, setUsersListsSelections] = useState({
+        "all": []
+    });
+    // The fields that should be currently displayed
     const [fields, setFields] = useState(Object.keys(FIELDS))
     
-    console.log("DATA", _querySets, _userLists)
-    return <DynamicDisplay querySets={_querySets} selectedList={list}>
-        <Table users={userLists[list]} fields={fields} />
+    // Updates the users selection for the current selected list
+    const setSelectedUsers = (list, users) => {
+        console.log("UPDATE users selection", users)
+        setUsersListsSelections({
+            ...usersListsSelections,
+            [list]: users
+        }) 
+    }
+    
+    // Filters the users for the current selected users list
+    const selectedUsers = userLists[list].results.filter((user) => usersListsSelections[list].indexOf(user.hash) != -1)
+    
+    return <DynamicDisplay querySets={_querySets} selectedList={list} selectedUsersHashes={usersListsSelections[list]} selectedUsers={selectedUsers}>
+        <Table users={userLists[list]} fields={fields} selectedUsers={usersListsSelections[list]} setSelectedUsers={(users) => setSelectedUsers(list, users)}/>
     </DynamicDisplay>
 }
