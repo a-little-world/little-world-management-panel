56111f97bf639def90504436e638db1c7eb7faa3
tim <tim@timschupp.de>
2023-07-23 18:42:44 +0200

    repared to enabled admin panel base data updating

--- src/AdminPanelV2.js
+++ src/AdminPanelV2.js
@@ -6,6 +6,14 @@ import { AdminPanel } from './panel_v2/AdminPanel';
 function AdminPanelV2(props) {
   const initData = props.data;
   const [data, setData] = useState(initData);
+  
+  /**
+   *  The admin pannel with gereally render with data,
+    *  but if we need to reload we may still use useSWR
+   */
+  //const fetcher = (...args) => fetch(...args).then(res => res.json());
+  //const { data: _data, error, isLoading } = useSWR(`/api/admin/user_advanced/${user.id}/`, fetcher)
+
   return (
     <AdminPanel _querySets={data?.query_sets} _userLists={data?.user_lists} />
   );
