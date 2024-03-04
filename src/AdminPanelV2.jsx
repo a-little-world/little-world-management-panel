4f03931a8a41936b67a8cbb1330eae008e098d39
Tim Schupp <tim@timschupp.de>
2024-03-04 15:52:03 +0100

    added simple test screen for websocket callbacks

--- src/AdminPanelV2.jsx
+++ src/AdminPanelV2.jsx
@@ -1,4 +1,4 @@
-import { useMemo,useEffect, useState } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 import { AdminPanel } from './panel_v2/AdminPanel';
@@ -8,10 +8,10 @@ import { createBrowserRouter, Outlet } from "react-router-dom";
 import { UserGroupIcon, ChartBarIcon, UserIcon, Mail } from '@heroicons/react/20/solid'
 import { AdminPanelV2_Matches } from './panel_v2/AdminPanelMatches';
 import { AdminPanelV2_Emails, AdminPanelV2_EmailDetails } from './panel_v2/AdminPanelEmails';
-import { DevkitSelector } from './panel_v2/AdminPanelDevkit';
+import { AdminPanelV2_DevKit, DevkitSelector } from './panel_v2/AdminPanelDevkit';
 
 export function HeroiconsSolidMail(props) {
-	return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" {...props}><g fill="currentColor"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0 0 16 4H4a2 2 0 0 0-1.997 1.884"></path><path d="m18 8.118l-8 4l-8-4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2z"></path></g></svg>);
+  return (<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" {...props}><g fill="currentColor"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0 0 16 4H4a2 2 0 0 0-1.997 1.884"></path><path d="m18 8.118l-8 4l-8-4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2z"></path></g></svg>);
 }
 
 
@@ -29,22 +29,22 @@ function Root() {
   return (
     <div className="flex justify-center items-center h-screen">
       <a href={`${BACKEND_PATH}/users/`} className="flex flex-col items-center bg-indigo-500 p-8 rounded-lg shadow-lg m-4 h-64 w-64">
-        <UserGroupIcon className="h-16 w-16 text-white mb-2"/>
+        <UserGroupIcon className="h-16 w-16 text-white mb-2" />
         <h2 className="text-white text-2xl">Users</h2>
       </a>
       <a href={`${BACKEND_PATH}/matches/`} className="flex flex-col items-center bg-green-500 p-8 rounded-lg shadow-lg m-4 h-64 w-64">
-        <UserIcon className="h-16 w-16 text-white mb-2"/>
+        <UserIcon className="h-16 w-16 text-white mb-2" />
         <h2 className="text-white text-2xl">Matches</h2>
       </a>
       <a href={`${BACKEND_PATH}/stats/`} className="flex flex-col items-center bg-yellow-500 p-8 rounded-lg shadow-lg m-4 h-64 w-64">
-        <ChartBarIcon className="h-16 w-16 text-white mb-2"/>
+        <ChartBarIcon className="h-16 w-16 text-white mb-2" />
         <h2 className="text-white text-2xl">Stats</h2>
       </a>
       <a href={`${BACKEND_PATH}/emails/`} className="flex flex-col items-center bg-blue-500 p-8 rounded-lg shadow-lg m-4 h-64 w-64">
-        <HeroiconsSolidMail className="h-16 w-16 text-white mb-2"/>
+        <HeroiconsSolidMail className="h-16 w-16 text-white mb-2" />
         <h2 className="text-white text-2xl">emails</h2>
       </a>
-      <DevkitSelector/>
+      <DevkitSelector />
     </div>
   )
 }
@@ -55,23 +55,23 @@ function AdminPanelV2_Users(props) {
   const [data, setData] = useState(null);
   const [selectedUsers, setSelectedUsers] = useState(initData?.selected_users ? initData.selected_users : []);
   console.log("User selection updated", selectedUsers)
-  
+
   /**
    *  The admin pannel with gereally render with data,
     *  but if we need to reload we may still use useSWR
    */
-  
+
   var queryParams = new URLSearchParams(window.location.search);
   let list = queryParams.get('list')
   list = list ? list : 'all'
   const fetcher = (...args) => fetch(...args).then(res => res.json());
-  const { data: _data, error, mutate,  isLoading } = useSWR(`/api/admin/user_listing_advanced/${list}/`, fetcher, {
+  const { data: _data, error, mutate, isLoading } = useSWR(`/api/admin/user_listing_advanced/${list}/`, fetcher, {
     initData: initData,
     revalidateOnMount: true
   })
-  
+
   console.log("DATATAATA", data, _data)
-  
+
   useEffect(() => {
     console.log("useEffect called", _data, isLoading, error)
     if (isLoading) {
@@ -84,17 +84,17 @@ function AdminPanelV2_Users(props) {
   }, [_data]);
 
   return (
-    data ? <AdminPanel 
-      _querySets={data?.query_sets} 
-      _userLists={data?.user_lists} 
+    data ? <AdminPanel
+      _querySets={data?.query_sets}
+      _userLists={data?.user_lists}
       reloadUserList={() => {
         console.log("Mutation called");
         mutate();
       }}
-      setData={setData} 
+      setData={setData}
       selectedUsers={selectedUsers}
       setSelectedUsers={setSelectedUsers}
-      initialList={list}/> : <div>Loading...</div>
+      initialList={list} /> : <div>Loading...</div>
   );
 }
 
@@ -123,6 +123,10 @@ function AdminPanelV2(props) {
             {
               path: "emails",
               element: <AdminPanelV2_Emails {...props} />,
+            },
+            {
+              path: "devkit",
+              element: <AdminPanelV2_DevKit {...props} />,
             },
             {
               path: "emails/:emailTemplateName",
