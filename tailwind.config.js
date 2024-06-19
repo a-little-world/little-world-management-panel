4644cf8373a489a637f34d4a7144ff36de2becb8
Tim Schupp <tim@timschupp.de>
2024-06-20 01:44:54 +0200

    add shadcnui dialog

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,40 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('daisyui')],
+  darkMode: ["class"],
+  content: [
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: "",
   theme: {
-    screens: { mid: '800px' },
+    screens: {
+      mid: "800px"
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       zIndex: {
-        1000: '1000',
+        1000: "1000",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-};
+  plugins: [require("tailwindcss-animate")],
+}
