7b826428a9653621d826115ea3e0d9d9f7cbd5d6
Tim <tim@timschupp.de>
2024-07-03 14:02:47 +0200

    date picker component and advanced stats rendering screen

--- src/views/Stats.tsx
+++ src/views/Stats.tsx
@@ -2,26 +2,48 @@ import React from 'react';
 import { cratePostFetcher } from '../store';
 import useSWR from 'swr';
 import DataGraph from '../blocks/DataGraph';
+import { DatePickerDemo } from '../atoms/DatePicker';
 
 
-function Stats() {
+function RangedDataGraph({
+    endpoint
+}) {
+
+    const [startDate, setStartDate] = React.useState('2022-01-01');
+    const [endDate, setEndDate] = React.useState('2024-01-01');
+    const [dayRange, setDayRange] = React.useState(1); // supports only 1 or 7 atm
 
     const {
-        data: sinupStats,
         mutate,
         error,
+        data,
         isLoading,
-    } = useSWR(`/api/matching/users/statistics/signups/`, cratePostFetcher({
-        start_date: '2024-01-01',
+    } = useSWR(endpoint, cratePostFetcher({
+        start_date: startDate,
+        end_date: endDate,
+        bucket_size: dayRange,
     }), {});
 
     if (isLoading) return <div>Loading...</div>;
 
+
+    return (
+        <div className="flex flex-col justify-center items-center h-screen">
+            <div>
+                settings
+            </div>
+            <h2>{endpoint}</h2>
+            <DataGraph data={data} />
+        </div>
+    )
+}
+
+function Stats() {
+
     return (
         <div className="flex justify-center items-center h-screen">
-            <h2>Stats</h2>
-            sdfs
-            <DataGraph data={sinupStats} />
+            <DatePickerDemo />
+            <RangedDataGraph endpoint="/api/matching/users/statistics/signups/" />
         </div>
     )
 }
