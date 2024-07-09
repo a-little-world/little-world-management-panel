d0c43b0fab54339af8a4a97a62a4f4554bb3a66a
Tim Schupp <tim@timschupp.de>
2024-07-09 13:08:39 +0200

    improved data graph component

--- src/blocks/DataGraph.tsx
+++ src/blocks/DataGraph.tsx
@@ -4,16 +4,6 @@ import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
 
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "../shadcnui/ui/chart"
 
-const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-]
-
-
 export function DataGraph({
     data,
     dataLabel
@@ -23,6 +13,10 @@ export function DataGraph({
         count: {
             label: dataLabel,
             color: "#2563eb",
+        },
+        date: {
+            label: "Date",
+            color: "#000",
         }
     };
 
@@ -40,7 +34,7 @@ export function DataGraph({
             />
             <ChartTooltip
                 cursor={false}
-                content={<ChartTooltipContent hideLabel />}
+                content={<ChartTooltipContent />}
             />
             <Bar dataKey="count" fill="var(--color-desktop)" radius={8} />
         </BarChart>
