ddfc5052e17945c4e3fdc36cdaeda59df302916d
Tim Schupp <tim@timschupp.de>
2024-07-09 13:08:19 +0200

    re-stuctured stats

--- src/views/Stats.tsx
+++ src/views/Stats.tsx
@@ -1,129 +1,38 @@
-import React from 'react';
-import { cratePostFetcher } from '../store';
-import useSWR from 'swr';
-import DataGraph from '../blocks/DataGraph';
-import { DatePicker } from '../atoms/DatePicker';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../shadcnui/ui/tabs";
+
 import { Dropdown, Text } from '@a-little-world/little-world-design-system';
+import { RangedDataGraph } from '../blocks/RangedDataGraph';
+import { BarChartTimeRanged } from '../blocks/BarChartTimeRanged';
 
-const StyledDropdown = styled(Dropdown)`
-  div[data-radix-popper-content-wrapper] {
-    z-index: 20 !important;
-  }
-  width: 100%;
-`;
-
-
-
-const graphEndpoints = [{
-    endpoint: "/api/matching/users/statistics/video_calls/",
-    title: "User Video Calls",
-    description: "The amount of video calls made in a given time period."
-}, {
-    endpoint: "/api/matching/users/statistics/signups/",
-    title: "User Signups",
-    description: "The amount of users that signed up in a given time period."
-}, {
-    endpoint: "/api/matching/users/statistics/messages_send/",
-    title: "User Messages",
-    description: "The amount of messages sent in a given time period."
-}]
-
-function RangedDataGraph() {
-
-    const [endpoint, setEndpoint] = React.useState(graphEndpoints[0]);
-
-    const [startDate, setStartDate] = React.useState('2024-01-01');
-    const today = new Date();
-    const [endDate, setEndDate] = React.useState(today.toISOString().split('T')[0]);
-    const [dayRange, setDayRange] = React.useState(1); // supports only 1 or 7 atm
-
-    const {
-        mutate,
-        error,
-        data,
-        isLoading,
-    } = useSWR(endpoint.endpoint, cratePostFetcher({
-        start_date: startDate,
-        end_date: endDate,
-        bucket_size: dayRange,
-    }), {});
-
-    if (isLoading) return <div>Loading...</div>;
-    if (!data) return <div>Error: {error}</div>;
-
-
-    return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <h2>{endpoint.endpoint}</h2>
-            <StyledDropdown
-                value={endpoint.endpoint}
-                options={graphEndpoints.map(({ endpoint, title, description }) => ({
-                    value: endpoint,
-                    label: description,
-                }))}
-                onValueChange={(val) => {
-                    setEndpoint(graphEndpoints.find(({ endpoint }) => endpoint === val) || graphEndpoints[0]);
-                }}
-                placeholder="Select a user list..."
-                cannotError
-            />
-            <span>NOTE: Any statistics are filtered down to the users the current matching user has access too</span>
-            <div className='w-full flex flex-row'>
-                <div className='flex flex-col items-center content-center justify-center'>
-                    <div className='flex w-full items-start'>
-                        Start Date:
-                    </div>
-                    <DatePicker date={startDate} setDate={(date) => {
-                        setStartDate(date);
-                        setTimeout(() => {
-                            mutate();
-                        }, 500);
-                    }} />
-                </div>
-                <div className='flex flex-col items-center content-center justify-center'>
-                    <div className='flex w-full items-start'>
-                        End Date
-                    </div>
-                    <DatePicker date={endDate} setDate={(date) => {
-                        setEndDate(date);
-                        setTimeout(() => {
-                            mutate();
-                        }, 500);
-                    }} />
-                </div>
-                <div className='flex flex-col items-center content-center justify-center'>
-                    <div className='flex w-full items-start'>
-                        Day Range:
-                    </div>
-                    <StyledDropdown
-                        value={dayRange.toString()}
-                        options={[1, 7, 30].map((val) => ({
-                            value: val.toString(),
-                            label: val === 1 ? 'Daily' : val === 7 ? 'Weekly' : 'Monthly',
-                        }))}
-                        onValueChange={(val) => {
-                            setDayRange(parseInt(val));
-                            setTimeout(() => {
-                                mutate();
-                            }, 500);
-                        }}
-                        placeholder="Select a user list..."
-                        cannotError
-                    />
-                </div>
-            </div>
-            <DataGraph data={data} dataLabel={`${endpoint.title}: `} />
-        </div>
-    )
-}
 
 function Stats() {
 
+    const [tab, setTab] = useState("graphs");
+
+    const onTabChange = (value: string) => {
+        setTab(value);
+    };
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <RangedDataGraph />
+        <div className="flex flex-col justify-center items-center h-screen w-full relative">
+            <Tabs value={tab} onValueChange={onTabChange} className="flex flex-col w-full h-full">
+                <TabsList>
+                    <TabsTrigger value="graphs">Graphs</TabsTrigger>
+                    <TabsTrigger value="charts">Charts</TabsTrigger>
+                    <TabsTrigger value="numbers">Numbers</TabsTrigger>
+                </TabsList>
+                {tab === "graphs" && <TabsContent value="graphs" className='flex flex-col content-center justify-center items-center flex-grow'>
+                    <RangedDataGraph />
+                </TabsContent>}
+                {tab === "charts" && <TabsContent value="charts" className='flex flex-col content-center justify-center items-center flex-grow'>
+                    <BarChartTimeRanged />
+                </TabsContent>}
+                {tab === "numbers" && <TabsContent value="numbers" className='flex flex-col content-center justify-center items-center flex-grow'>
+                    Hello?
+                </TabsContent>}
+            </Tabs>
         </div>
     )
 }
