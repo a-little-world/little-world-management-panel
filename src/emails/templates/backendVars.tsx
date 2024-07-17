d7756d145295e42002ed3c568b5ea2f17ce25118
Tim Schupp <tim@timschupp.de>
2024-07-17 13:42:16 +0200

    example template setup

---
+++ src/emails/templates/backendVars.tsx
@@ -0,0 +1,6 @@
+export const BackendVars = {
+    firstName: '{{ first_name }}',
+    secondName: '{{ second_name }}',
+    email: '{{ email }}',
+    verificationCode: '{{ verification_code }}',
+}
