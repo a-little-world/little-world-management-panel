ecc08a827c49600745bd97651730d35f3807e70b
Tim Schupp <tim@timschupp.de>
2026-01-23 16:49:34 +0100

    added new session graph endpoint

--- src/components/blocks/stats/RangedDataGraph.tsx
+++ src/components/blocks/stats/RangedDataGraph.tsx
@@ -26,6 +26,11 @@ export const graphEndpoints = [
     description: 'The amount of users that signed up in a given time period.',
   },
   {
+    endpoint: '/api/matching/users/statistics/sessions/',
+    title: 'User Sessions',
+    description: 'The amount of user sessions created in a given time period.',
+  },
+  {
     endpoint: '/api/matching/users/statistics/signups/?cumulative=true',
     title: 'cummulative user signups',
     description:
@@ -93,7 +98,7 @@ export function RangedDataGraph() {
         onValueChange={val => {
           setEndpoint(
             graphEndpoints.find(({ endpoint }) => endpoint === val) ||
-              graphEndpoints[0],
+            graphEndpoints[0],
           );
         }}
         placeholder="Select a user list..."
